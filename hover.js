58027d388a82e9dbbcaf0c3b4c96e9bafc5f2748
Carlos Torres <8ctorres@stpatrick.org>
2017-04-21 10:49:30 -0500

    Add files via upload

--- hover.js
+++ hover.js
@@ -1,11 +1,43 @@
-(function hoverimg(){
-  const doc = document
-  let img = doc.querySelector("div.mainimg img")
+(function(){
+  const doc = document;
+  let img = doc.querySelector(".mainimg");
 
-  img.addEventListener("mouseOver")
-  img.style.border-color = "00ff00"
+  img.addEventListener("mouseover",function(){
+  img.style.borderColor = "#00ff00";
+});
 
-  img.addEventListener("mouseOut")
-  img.style.border-color = "ffffff"
+  img.addEventListener("mouseout",function(){
+  img.style.borderColor = "#ffffff";
+});
 
-})()
+})();
+
+(function(){
+
+  const doc = document;
+  let leftbutton = doc.querySelector("#left");
+
+leftbutton.addEventListener("mouseover",function(){
+  leftbutton.style.opacity = "0.8";
+});
+
+leftbutton.addEventListener("mouseout",function(){
+  leftbutton.style.opacity = "1";
+});
+
+})();
+
+(function(){
+
+  const doc = document;
+  let rightbutton = doc.querySelector("#right");
+
+rightbutton.addEventListener("mouseover",function(){
+  rightbutton.style.opacity = "0.8";
+});
+
+rightbutton.addEventListener("mouseout",function(){
+  rightbutton.style.opacity = "1";
+});
+
+})();
