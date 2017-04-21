2d7041466c0e3daa221ffdcff8f68ce3887ca294
Carlos Torres <8ctorres@stpatrick.org>
2017-04-21 10:55:22 -0500

    Add files via upload

--- hover.js
+++ hover.js
@@ -1,19 +1,20 @@
 (function(){
+//green border around main image when mouse over it
   const doc = document;
   let img = doc.querySelector(".mainimg");
 
-  img.addEventListener("mouseover",function(){
+img.addEventListener("mouseover",function(){
   img.style.borderColor = "#00ff00";
 });
 
-  img.addEventListener("mouseout",function(){
-  img.style.borderColor = "#ffffff";
+img.addEventListener("mouseout",function(){
+  img.style.borderColor = "ffffff";
 });
 
 })();
 
 (function(){
-
+//diminish opacity of left button when mouse over it
   const doc = document;
   let leftbutton = doc.querySelector("#left");
 
@@ -28,7 +29,7 @@ leftbutton.addEventListener("mouseout",function(){
 })();
 
 (function(){
-
+//diminish opacity of right button when mouse over it
   const doc = document;
   let rightbutton = doc.querySelector("#right");
 
@@ -38,6 +39,21 @@ rightbutton.addEventListener("mouseover",function(){
 
 rightbutton.addEventListener("mouseout",function(){
   rightbutton.style.opacity = "1";
+});
+
+})();
+
+(function(){
+
+  const doc = document;
+  let phome = doc.querySelector("#phome");
+
+phome.addEventListener("mouseover",function(){
+  phome.style.underline =
+});
+
+phome.addEventListener("mouseout",function(){
+  phome.style.underline =
 });
 
 })();
