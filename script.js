9c74bf41a3efeb3281774525a3eae361d81e7719
Carlos Torres <8ctorres@stpatrick.org>
2017-04-23 13:52:32 -0500

    Added more code

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 (function(){
 //green border around main image when mouse over it
   const doc = document;
-  let img = doc.querySelector(".mainimg img");
+  let img = doc.querySelector(".mainimg img#frontimg");
 
 img.addEventListener("mouseover",function(){
   img.style.border = "20px solid green";
+  img.style.backgroundColor = "green";
 });
 
 img.addEventListener("mouseout",function(){
-  img.style.border = "20px solid white";
+  img.style.border = "20px solid transparent";
+  img.style.backgroundColor = "transparent";
 });
 
 })();
@@ -61,7 +63,7 @@ phome.addEventListener("mouseout",function(){
 (function(){
 //underline about in nav bar
   const doc = document;
-  let phome = doc.querySelector("#pabout");
+  let pabout = doc.querySelector("#pabout");
 
 pabout.addEventListener("mouseover",function(){
   pabout.style.textDecoration = "underline";
@@ -71,4 +73,323 @@ pabout.addEventListener("mouseout",function(){
   pabout.style.textDecoration = "none";
 });
 
+})();
+
+(function(){
+//underline downloads in nav bar
+  const doc = document;
+  let pdownloads = doc.querySelector("#pdownloads");
+
+pdownloads.addEventListener("mouseover",function(){
+  pdownloads.style.textDecoration = "underline";
+});
+
+pdownloads.addEventListener("mouseout",function(){
+  pdownloads.style.textDecoration = "none";
+});
+
+})();
+
+(function(){
+//underline docs in nav bar
+  const doc = document;
+  let pdocs = doc.querySelector("#pdocs");
+
+pdocs.addEventListener("mouseover",function(){
+  pdocs.style.textDecoration = "underline";
+});
+
+pdocs.addEventListener("mouseout",function(){
+  pdocs.style.textDecoration = "none";
+});
+
+})();
+
+(function(){
+//underline foundation in nav bar
+  const doc = document;
+  let pfoundation = doc.querySelector("#pfoundation");
+
+pfoundation.addEventListener("mouseover",function(){
+  pfoundation.style.textDecoration = "underline";
+});
+
+pfoundation.addEventListener("mouseout",function(){
+  pfoundation.style.textDecoration = "none";
+});
+
+})();
+
+(function(){
+//underline getinvolved in nav bar
+  const doc = document;
+  let pgetinvolved = doc.querySelector("#pgetinvolved");
+
+pgetinvolved.addEventListener("mouseover",function(){
+  pgetinvolved.style.textDecoration = "underline";
+});
+
+pgetinvolved.addEventListener("mouseout",function(){
+  pgetinvolved.style.textDecoration = "none";
+});
+
+})();
+
+(function(){
+//underline security in nav bar
+  const doc = document;
+  let psecurity = doc.querySelector("#psecurity");
+
+psecurity.addEventListener("mouseover",function(){
+  psecurity.style.textDecoration = "underline";
+});
+
+psecurity.addEventListener("mouseout",function(){
+  psecurity.style.textDecoration = "none";
+});
+})();
+
+(function(){
+//underline news in nav bar
+  const doc = document;
+  let pnews = doc.querySelector("#pnews");
+
+pnews.addEventListener("mouseover",function(){
+  pnews.style.textDecoration = "underline";
+});
+
+pnews.addEventListener("mouseout",function(){
+  pnews.style.textDecoration = "none";
+});
+})();
+
+(function(){
+  //underline other downloads under left button
+  const doc = document;
+  let text = doc.querySelector("#otherleft");
+
+text.addEventListener("mouseover",function(){
+  text.style.textDecoration = "underline";
+});
+
+text.addEventListener("mouseout",function(){
+  text.style.textDecoration = "none";
+
+});
+})();
+
+(function(){
+  //underline changelog under left button
+  const doc = document;
+  let text = doc.querySelector("#changelogleft");
+
+text.addEventListener("mouseover",function(){
+  text.style.textDecoration = "underline";
+});
+
+text.addEventListener("mouseout",function(){
+  text.style.textDecoration = "none";
+
+});
+})();
+
+(function(){
+  //underline API under left button
+  const doc = document;
+  let text = doc.querySelector("#apileft");
+
+text.addEventListener("mouseover",function(){
+  text.style.textDecoration = "underline";
+});
+
+text.addEventListener("mouseout",function(){
+  text.style.textDecoration = "none";
+
+});
+})();
+
+(function(){
+  //underline other downloads under right button
+  const doc = document;
+  let text = doc.querySelector("#otherright");
+
+text.addEventListener("mouseover",function(){
+  text.style.textDecoration = "underline";
+});
+
+text.addEventListener("mouseout",function(){
+  text.style.textDecoration = "none";
+
+});
+})();
+
+(function(){
+  //underline changelog under right button
+  const doc = document;
+  let text = doc.querySelector("#changelogright");
+
+text.addEventListener("mouseover",function(){
+  text.style.textDecoration = "underline";
+});
+
+text.addEventListener("mouseout",function(){
+  text.style.textDecoration = "none";
+
+});
+})();
+
+(function(){
+  //underline API under right button
+  const doc = document;
+  let text = doc.querySelector("#apiright");
+
+text.addEventListener("mouseover",function(){
+  text.style.textDecoration = "underline";
+});
+
+text.addEventListener("mouseout",function(){
+  text.style.textDecoration = "none";
+
+});
+})();
+
+(function(){
+  //underline report nodejs in footer
+  const doc = document;
+  let text = doc.querySelector("#reportnodejs");
+
+text.addEventListener("mouseover",function(){
+  text.style.textDecoration = "underline";
+});
+
+text.addEventListener("mouseout",function(){
+  text.style.textDecoration = "none";
+
+});
+})();
+
+(function(){
+  //underline report website in footer
+  const doc = document;
+  let text = doc.querySelector("#reportwebsite");
+
+text.addEventListener("mouseover",function(){
+  text.style.textDecoration = "underline";
+});
+
+text.addEventListener("mouseout",function(){
+  text.style.textDecoration = "none";
+
+});
+})();
+
+(function(){
+  //underline get help in footer
+  const doc = document;
+  let text = doc.querySelector("#gethelp");
+
+text.addEventListener("mouseover",function(){
+  text.style.textDecoration = "underline";
+});
+
+text.addEventListener("mouseout",function(){
+  text.style.textDecoration = "none";
+
+});
+})();
+
+(function(){
+  //underline trademarkguidelines in footer
+  const doc = document;
+  let text = doc.querySelector("#trademarkguidelines");
+
+text.addEventListener("mouseover",function(){
+  text.style.textDecoration = "underline";
+});
+
+text.addEventListener("mouseout",function(){
+  text.style.textDecoration = "none";
+
+});
+})();
+
+(function(){
+  //underline trademark in footer
+  const doc = document;
+  let text = doc.querySelector("#trademark");
+
+text.addEventListener("mouseover",function(){
+  text.style.textDecoration = "underline";
+});
+
+text.addEventListener("mouseout",function(){
+  text.style.textDecoration = "none";
+
+});
+})();
+
+(function(){
+  //underline nodejslicensing in footer
+  const doc = document;
+  let text = doc.querySelector("#nodejslicensing");
+
+text.addEventListener("mouseover",function(){
+  text.style.textDecoration = "underline";
+});
+
+text.addEventListener("mouseout",function(){
+  text.style.textDecoration = "none";
+
+});
+})();
+
+(function(){
+  //highlight in green LTS Shchedule
+  const doc = document;
+  let text = doc.querySelector("#ltsschedule");
+
+text.addEventListener("mouseover",function(){
+  text.style.color = "white";
+  text.style.backgroundColor = "green";
+});
+
+text.addEventListener("mouseout",function(){
+  text.style.color = "#363";
+  text.style.backgroundColor = "transparent";
+
+});
+})();
+
+(function(){
+  //highlight in green chrome
+  const doc = document;
+  let text = doc.querySelector("#chrome");
+
+text.addEventListener("mouseover",function(){
+  text.style.color = "white";
+  text.style.backgroundColor = "green";
+});
+
+text.addEventListener("mouseout",function(){
+  text.style.color = "#363";
+  text.style.backgroundColor = "transparent";
+
+});
+})();
+
+(function(){
+  //highlight in green npm
+  const doc = document;
+  let text = doc.querySelector("#npm");
+
+text.addEventListener("mouseover",function(){
+  text.style.color = "white";
+  text.style.backgroundColor = "green";
+});
+
+text.addEventListener("mouseout",function(){
+  text.style.color = "#363";
+  text.style.backgroundColor = "transparent";
+
+});
 })();
